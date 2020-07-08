dc30efb66381c91100cbe717056a26960838a2d9
julimancan <julian.m.bustos@gmail.com>
2020-07-08 13:07:52 -0400

    got the form to send the data back up

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -15,6 +15,10 @@ export default function Form(props) {
     props.onCancel();
   }
 
+  const save = () => {
+    props.onSave(name, interviewer);
+  }
+
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
@@ -36,7 +40,7 @@ export default function Form(props) {
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <Button onClick={cancel} danger>Cancel</Button>
-          <Button onClick={props.onSave} confirm>Save</Button>
+          <Button onClick={save} confirm>Save</Button>
         </section>
       </section>
     </main>
