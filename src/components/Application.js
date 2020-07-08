71b4d86796d0c3929b7c0f9d982b58428f6d524c
julimancan <julian.m.bustos@gmail.com>
2020-07-08 13:55:25 -0400

    added a map for the appointments but results not showing correctly

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,6 +3,62 @@ import React, { useState } from "react";
 
 import "components/Application.scss";
 import DayList from "components/DayList.js";
+import Appointment from "components/Appointment";
+
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "2pm",
+    interview: {
+      student: "Jose Feliciano",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Willy Colon",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 5,
+    time: "4pm",
+    interview: {
+      student: "Victor Manuel",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  }
+];
 
 
 const interviewer = {
@@ -32,6 +88,20 @@ const days = [
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
 
+  const appointment = appointments.map((appointment, index) => {
+    return <Appointment
+    key={appointment.id} {...appointment}
+    // id={appointment.id}
+    // time={appointment.time}
+    // // interview={{ student: "Lydia Miller-Jones", interviewer }}
+    // interview={{...appointment.interview}}
+
+    // onEdit={action("onEdit")}
+    // onDelete={action("onDelete")}
+  />
+  // <Appointment id="last" time="1pm" />
+  })
+
   return (
     <main className="layout">
       <section className="sidebar">
@@ -54,6 +124,10 @@ export default function Application(props) {
         />      </section>
       <section className="schedule">
         {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+
+        {appointment}
+        <Appointment key="last" time="5pm" />
+
       </section>
     </main>
   );
