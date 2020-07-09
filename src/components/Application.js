e6bfd4025008de2507ab953899b0d4ba87167673
julimancan <julian.m.bustos@gmail.com>
2020-07-08 22:32:17 -0400

    added promises to handle the interviews in the days and displayed them correctly on the webApp

--- src/components/Application.js
+++ src/components/Application.js
@@ -6,6 +6,7 @@ import "components/Application.scss";
 import DayList from "components/DayList.js";
 import Appointment from "components/Appointment";
 import axios from 'axios';
+import { getAppointmentsForDay } from "helpers/selectors"
 
 const appointments = [
   {
@@ -86,23 +87,50 @@ const interviewer = {
 //     spots: 0,
 //   },
 // ];
+let appointment = []
 
 export default function Application(props) {
-  const [days, setDays] = useState([]);
-  const [day, setDay] = useState("Monday");
+  // const [days, setDays] = useState([]);
+  // const [day, setDay] = useState("Monday");
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {}
+  });
 
-  const appointment = appointments.map((appointment, index) => {
-    return <Appointment
-              key={appointment.id} {...appointment}
-           />
-  })
+  const setDay = day => setState(prev => {
+    console.log("set day!!!!!!!!", prev, day)
+    const appointmentsForDay = getAppointmentsForDay(prev, day)
+    console.log(appointmentsForDay, "appointments for the day")
+   appointment = getAppointmentsForDay(prev, day).map((appointment, index) => {
+      return <Appointment
+        key={appointment.id} {...appointment}
+      />
+    })
+    return ({ ...prev, day })
+});
+  // const setDays = days => setState(prev => ({...prev, days}));
+
+
 
   useEffect(() => {
-    axios
-      .get("/api/days")
-      .then(({data}) => setDays(data))
-      .catch(err => console.log(err))
-      .finally(console.log("Got days from days API"))
+    // axios
+    //   .get("/api/days")
+    //   // .then(({ data }) => setDays(data))
+    //   .catch(err => console.log(err))
+    //   .finally(console.log("Got days from days API"))
+    const getDays = axios.get("/api/days")
+    const getAppointments = axios.get("/api/appointments")
+    
+    Promise.all([getDays, getAppointments, Promise.resolve(appointments)])
+      .then( all  => {
+        console.log('---------', all)
+        setState(prev => ({...prev, days: all[0].data, appointments: all[1].data }));
+        setDay("Monday")
+        // console.log('--------------------------------------------->', getAppointments)})
+      })
+      
+  
   }, [])
 
   return (
@@ -115,8 +143,8 @@ export default function Application(props) {
         />
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu"> <DayList
-          days={days}
-          day={day}
+          days={state.days}
+          day={state.day}
           setDay={setDay}
         />
         </nav>
--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -3,27 +3,19 @@ import React, { useState, useEffect } from "react";
 
 
 export function getAppointmentsForDay(state, day) {
-  // console.log('00000000000000000000000000000000000000', state)
   const [filteredDay] = state.days.filter(d => d.name === day);
-// put a conditional to filter the undefines and return an empty array
-if (filteredDay) {
-const appointments = filteredDay.appointments
-  // const appointments = filteredDays.appointments.filter(d => d.id === states.appointments);
-  // console.log('==================================================>>>>>>>>>>>>', appointments)
-  const listOfAppointmentObjs = []
-  for (let appointmentId of appointments) {
-    if (appointmentId in state.appointments) {
-      listOfAppointmentObjs.push(state.appointments[appointmentId])
+  if (filteredDay) {
+    const appointments = filteredDay.appointments
+    const listOfAppointmentObjs = []
+    for (let appointmentId of appointments) {
+      if (appointmentId in state.appointments) {
+        listOfAppointmentObjs.push(state.appointments[appointmentId])
+      }
     }
+    return listOfAppointmentObjs
   }
-  // let filteredList = filteredDays.appointments.filter(day => {
-  //   return day === filteredDays.id;
-  // })
-  console.log('lllllllllllllllllllllllllllllll=============================<>', listOfAppointmentObjs)
-  return listOfAppointmentObjs
-}
-else {
-  return [];
-}
+  else {
+    return [];
+  }
 }
 
